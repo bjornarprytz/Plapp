9784b0f6b344fe845f5aff5e6e1e0dada7489663
bjornarprytz <bjornarprytz@users.noreply.github.com>
2020-10-13 23:26:46 +0200

    Started on styles. Consider removing some of the unneeded fonts.

--- Plapp/Plapp/App.xaml.cs
+++ Plapp/Plapp/App.xaml.cs
@@ -3,6 +3,19 @@ using Plapp.Core;
 using Plapp.Persist;
 using Xamarin.Forms;
 
+[assembly: ExportFont("SourceSansPro-Black.otf", Alias = "Black")]
+[assembly: ExportFont("SourceSansPro-BlackIt.otf", Alias = "BlackIt")]
+[assembly: ExportFont("SourceSansPro-Bold.otf", Alias = "Bold")]
+[assembly: ExportFont("SourceSansPro-BoldIt.otf", Alias = "BoldId")]
+[assembly: ExportFont("SourceSansPro-ExtraLight.otf", Alias = "ExtraLight")]
+[assembly: ExportFont("SourceSansPro-ExtraLightIt.otf", Alias = "ExtraLightIt")]
+[assembly: ExportFont("SourceSansPro-It.otf", Alias = "It")]
+[assembly: ExportFont("SourceSansPro-Light.otf", Alias = "Light")]
+[assembly: ExportFont("SourceSansPro-LightIt.otf", Alias = "LightIt")]
+[assembly: ExportFont("SourceSansPro-Regular.otf", Alias = "Regular")]
+[assembly: ExportFont("SourceSansPro-Semibold.otf", Alias = "Semibold")]
+[assembly: ExportFont("SourceSansPro-SemiboldIt.otf", Alias = "SemiboldIt")]
+
 namespace Plapp
 {
     public partial class App : Application
