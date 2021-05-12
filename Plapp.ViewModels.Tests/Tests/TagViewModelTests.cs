342aa385fb262268a5bf6c6d7ebdeb5bdd469dc6
Bjørnar Prytz <bjornarprytz@gmail.com>
2021-05-12 19:05:03 +0200

    Added test for TagViewModel

---
+++ Plapp.ViewModels.Tests/Tests/TagViewModelTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plapp.Core;
+using System;
+
+namespace Plapp.ViewModels.Tests
+{
+    [TestClass]
+    public class TagViewModelTests : BaseViewModelTests<TagViewModel>
+    {
+        protected override TagViewModel SetUpVM()
+        {
+            return new TagViewModel();
+        }
+
+        [TestMethod]
+        public void Hydrate_PopulatesAllFields()
+        {
+            var tag = new Tag
+            {
+                Id = 1,
+                Icon = Icon.Diamond,
+                Color = "some color",
+                DataType = DataType.Decimal,
+                Key = "some key",
+                Unit = "some unit",
+            };
+
+            VM.Hydrate(tag);
+
+            VM.Should().BeEquivalentTo(
+                new TagViewModel
+                {
+                    Id = 1,
+                    Icon = Icon.Diamond,
+                    Color = "some color",
+                    DataType = DataType.Decimal,
+                    Key = "some key",
+                    Unit = "some unit"
+                });
+        }
+    }
+}
