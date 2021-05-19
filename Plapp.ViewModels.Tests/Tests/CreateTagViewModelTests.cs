0fdedd124e9122fb299e2b5789b72e058289cd3d
Bjørnar Prytz <bjornarprytz@gmail.com>
2021-05-19 14:17:53 +0200

    Added a failing test, but it shows some issues with the way I map between DomainObjects and ViewModels

--- Plapp.ViewModels.Tests/Tests/CreateTagViewModelTests.cs
+++ Plapp.ViewModels.Tests/Tests/CreateTagViewModelTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Plapp.Core;
 using System;
@@ -12,17 +13,45 @@ namespace Plapp.ViewModels.Tests
     [TestClass]
     public class CreateTagViewModelTests : BaseCreateViewModelTests<CreateTagViewModel, ITagViewModel>
     {
+        private Mock<ITagService> tagServiceMock;
+
         protected override CreateTagViewModel SetUpVM()
         {
             base.SetUpVM();
 
-            var tagServiceMock = new Mock<ITagService>();
+            tagServiceMock = new Mock<ITagService>();
 
             return new CreateTagViewModel(
                 factoryMock.Object,
                 prompterMock.Object,
                 tagServiceMock.Object
                 );
+        }
+
+        [TestMethod]
+        public async Task LoadDataCommand_TagsAreLoaded()
+        {
+            await VM.LoadDataCommand.ExecuteAsync();
+
+            tagServiceMock.Verify(s => s.FetchAllAsync(default), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task LoadDataCommand_AvailableTagsAreAddedToCollection()
+        {
+            factoryMock.Setup(f => f()).Returns(() => new Mock<ITagViewModel>().Object);
+
+            var tag1 = new Tag { Id = 1 };
+            var tag2 = new Tag { Id = 2 };
+
+            var tags = new List<Tag> { tag1, tag2 };
+
+            tagServiceMock.Setup(s => s.FetchAllAsync(default)).Returns(Task.FromResult(tags.AsEnumerable()));
+
+            await VM.LoadDataCommand.ExecuteAsync();
+
+            VM.AvailableTags.Should().Contain(tag => tag.Id == 1);
+            VM.AvailableTags.Should().Contain(tag => tag.Id == 2);
         }
 
         // TODO: Test OnShow(), that LoadAvailableTags is called
