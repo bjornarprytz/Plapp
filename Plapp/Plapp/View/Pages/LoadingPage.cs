a41b4b617b04630515dd220d61675b8f6aaf5ba3
Bjørnar Prytz <bjornarprytz@gmail.com>
2021-05-20 00:32:24 +0200

    Added a silly animation to the loading page, but I don't think it'll be visible because Navigation will interrupt it.

--- Plapp/Plapp/View/Pages/LoadingPage.cs
+++ Plapp/Plapp/View/Pages/LoadingPage.cs
@@ -1,22 +1,38 @@
 ﻿using Plapp.ViewModels;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Plapp
 {
     class LoadingPage : BaseContentPage<LoadingViewModel>
     {
+        Label label;
+
         public LoadingPage()
         {
+            label = new Label
+            {
+                Text = "Loading",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+            };
+
             Content = new Grid
             {
                 Children =
                 {
-                    new Label
-                    { 
-                        Text  = "Loading"
-                    }
+                    label
                 }
             };
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+            await label.SillyAnimation();
         }
     }
 }
