8ef4c23c210a0ef8b564219e89d7ba7c7e9717ab
Bjørnar Prytz <bjornarprytz@gmail.com>
2021-05-19 12:45:12 +0200

    Added some tests for IOViewModel

---
+++ Plapp.ViewModels.Tests/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plapp.ViewModels.Tests
+{
+    public static class Extensions
+    {
+        public static void ListenToPropertyChanged<T>(this T thing, string propertyName, Action result)
+            where T : INotifyPropertyChanged
+        {
+            thing.PropertyChanged += Delegate;
+
+            void Delegate(object s, PropertyChangedEventArgs args)
+            {
+                if (args.PropertyName == propertyName)
+                {
+                    result();
+                    thing.PropertyChanged -= Delegate;
+                }
+            }
+        }
+    }
+}
--- Plapp.ViewModels.Tests/Tests/Base/IOViewModelTests.cs
+++ Plapp.ViewModels.Tests/Tests/Base/IOViewModelTests.cs
@@ -8,6 +8,28 @@ namespace Plapp.ViewModels.Tests
     public abstract class IOViewModelTests<TViewModel> : BaseViewModelTests<TViewModel>
         where TViewModel : IOViewModel
     {
+        [TestMethod]
+        public void OnShow_IsLoadingDataChanged()
+        {
+            bool IsLoadingDataChanged = false;
 
+            VM.ListenToPropertyChanged(nameof(VM.IsLoadingData), () => IsLoadingDataChanged = true);
+
+            VM.OnShow();
+
+            IsLoadingDataChanged.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void OnUserInteractionStopped_IsSavingDataChanged()
+        {
+            bool IsSavingDataChanged = false;
+
+            VM.ListenToPropertyChanged(nameof(VM.IsSavingData), () => IsSavingDataChanged = true);
+
+            VM.OnUserInteractionStopped();
+
+            IsSavingDataChanged.Should().BeTrue();
+        }
     }
 }
