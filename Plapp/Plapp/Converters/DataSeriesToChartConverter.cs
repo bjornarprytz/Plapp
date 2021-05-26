c001f49a21c31c3aa5677ac8cc74aff0b1c7469c
Bjørnar Prytz <bjornarprytz@gmail.com>
2021-05-26 13:04:38 +0200

    Updated the Chart converter and updated packages

--- Plapp/Plapp/Converters/DataSeriesToChartConverter.cs
+++ Plapp/Plapp/Converters/DataSeriesToChartConverter.cs
@@ -1,5 +1,7 @@
 ﻿using Microcharts;
 using Plapp.Core;
+using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,19 +10,34 @@ namespace Plapp
     public class DataSeriesToChartConverter<T> : BaseValueConverter<IEnumerable<IDataPointViewModel>, T>
         where T : Chart, new()
     {
+        readonly Action<T> _setState;
+
+        public DataSeriesToChartConverter(Action<T> setState=null)
+        {
+            _setState = setState;
+        }
+
         protected override T Convert(IEnumerable<IDataPointViewModel> from)
         {
             var chart = new T
             {
-                Entries = from.OrderBy(dp => dp.Date).Select(dp => DataPointToChartEntry(dp))
+                Entries = from.OrderBy(dp => dp.Date).Select(dp => DataPointToChartEntry(dp)),
+                BackgroundColor = SKColors.Transparent,
             };
+
+            _setState?.Invoke(chart);
 
             return chart;
         }
 
         private ChartEntry DataPointToChartEntry(IDataPointViewModel dataPointViewModel)
         {
-            return new ChartEntry(dataPointViewModel.Value);
+            return new ChartEntry(dataPointViewModel.Value) 
+            {
+                Color = SKColors.White,
+                TextColor = SKColors.White,
+                ValueLabel = dataPointViewModel.Value.ToString(),
+            };
         }
     }
 }
--- Plapp/Plapp/View/Components/DataSeriesGraph.cs
+++ Plapp/Plapp/View/Components/DataSeriesGraph.cs
@@ -22,7 +22,13 @@ namespace Plapp
                     {
                         new ChartView()
                             .Bind(IsVisibleProperty, nameof(VM.DataPoints), convert: (IEnumerable<IDataPointViewModel> c) => c.Any())
-                            .Bind(ChartView.ChartProperty, nameof(VM.DataPoints), converter: new DataSeriesToChartConverter<LineChart>()),
+                            .Bind(ChartView.ChartProperty, nameof(VM.DataPoints), converter: new DataSeriesToChartConverter<LineChart>(
+                                chart => 
+                                {
+                                    chart.LineMode = LineMode.Straight;
+                                    chart.PointMode = PointMode.Circle;
+                                    chart.LineSize = 1;
+                                })),
                         
                         new Button()
                             .MaterialIcon(MaterialDesign.Icons.MaterialIcon.Add)
