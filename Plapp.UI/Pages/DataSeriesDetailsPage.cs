7574990fa007df5a0394bd3b8703474e28bc3b5c
Bjørnar Prytz <bjornarprytz@gmail.com>
2021-09-01 19:18:21 +0200

    Some data point details.

--- Plapp.UI/Pages/DataSeriesDetailsPage.cs
+++ Plapp.UI/Pages/DataSeriesDetailsPage.cs
@@ -1,23 +1,54 @@
 using System.Reactive.Disposables;
 using Plapp.Core;
+using Plapp.UI.ContentViews;
 using ReactiveUI;
+using Xamarin.CommunityToolkit.Markup;
 using Xamarin.Forms;
 
 namespace Plapp.UI.Pages
 {
     public class DataSeriesDetailsPage : BaseContentPage<IDataSeriesViewModel>
     {
-        private readonly Button _button = new(){ Text = "Data series" };
+        private readonly DataSeriesGraph _graph = new();
+
+        private readonly Label _tag;
+        private readonly TapGestureRecognizer _tapTagGesture = new();
+
+        private readonly Slider _newDataPointSlider = new();
+        private readonly Button _addDataPointButton = new();
+        
         
         public DataSeriesDetailsPage()
         {
-            Content = _button;
+            _tag = new Label { GestureRecognizers = { _tapTagGesture } };
+            
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    _graph,
+                    _tag,
+                    
+                    _newDataPointSlider,
+                    _addDataPointButton
+                }
+            };
         }
         
         protected override void DoBindings(CompositeDisposable bindingsDisposable)
         {
-            this.OneWayBind(this.ViewModel, dataSeries => dataSeries.Tag.Key, page => page._button.Text)
+            this.OneWayBind(ViewModel, dataSeries => dataSeries, page => page._graph.BindingContext);
+            
+            this.OneWayBind(ViewModel, dataSeries => dataSeries.Tag.Key, page => page._tag.Text)
                 .DisposeWith(bindingsDisposable);
+
+            this.BindCommand(ViewModel, dataSeries => dataSeries.PickTagCommand, page => page._tapTagGesture)
+                .DisposeWith(bindingsDisposable);
+            
+            this.BindCommand(ViewModel, dataSeries => dataSeries.AddDataPointCommand, page => page._addDataPointButton)
+                .DisposeWith(bindingsDisposable);
+
+            
         }
     }
 }
