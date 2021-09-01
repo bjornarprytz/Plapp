7beb958dd6f12ce0da6b821f4d0385fa87182ea0
Bjørnar Prytz <bjornarprytz@gmail.com>
2021-09-01 12:12:14 +0200

    Added back fonts

--- Plapp.UI/Constants/Fonts.cs
+++ Plapp.UI/Constants/Fonts.cs
@@ -2,6 +2,13 @@ namespace Plapp.UI.Constants
 {
     public static class Fonts
     {
+        // SourceSans
+        public const string Bold = "SS-Bold";
+        public const string Black = "SS-Black";
+        public const string Itaclic = "SS-Italic";
+        public const string Light = "SS-Light";
+        public const string Regular = "SS-Regular";
+        
         // Material Icons (https://material.io/resources/icons/?style=baseline)
         public const string MIOutline = "MI-Outline";
         public const string MI = "MI";
--- Plapp/Plapp/AssemblyInfo.cs
+++ Plapp/Plapp/AssemblyInfo.cs
@@ -1,4 +1,19 @@
+using Plapp.UI.Constants;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
+// Fonts
+[assembly: ExportFont("SourceSansPro-Black.otf", Alias = Fonts.Black)]
+[assembly: ExportFont("SourceSansPro-Bold.otf", Alias = Fonts.Bold)]
+[assembly: ExportFont("SourceSansPro-It.otf", Alias = Fonts.Itaclic)]
+[assembly: ExportFont("SourceSansPro-Light.otf", Alias = Fonts.Light)]
+[assembly: ExportFont("SourceSansPro-Regular.otf", Alias = Fonts.Regular)]
+
+// Icons
+[assembly: ExportFont("MaterialIconsOutlined-Regular.otf", Alias = Fonts.MIOutline)]
+[assembly: ExportFont("MaterialIcons-Regular.ttf", Alias = Fonts.MI)]
+[assembly: ExportFont("MaterialIconsRound-Regular.otf", Alias = Fonts.MIRound)]
+[assembly: ExportFont("MaterialIconsSharp-Regular.otf", Alias = Fonts.MISharp)]
+[assembly: ExportFont("MaterialIconsTwoTone-Regular.otf", Alias = Fonts.MITwoTone)]
